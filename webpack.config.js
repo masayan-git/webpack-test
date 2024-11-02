5a1b1b421f07720c4ae08c0a43d54756ea396dcb
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-11-02 14:46:39 +0900

    load images with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { name } = require('file-loader');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -21,6 +22,18 @@ module.exports = {
             loader: 'css-loader',
           },
         ]
+      },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
